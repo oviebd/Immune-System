3bad061dc793680e29eefc49371789d425d63cc1
oviebd <oviecse48@gmail.com>
2020-02-21 23:54:18 +0600

    Add some graphics and a script for testing.

---
+++ Assets/Script/MoveCircular.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCircular : MonoBehaviour
+{
+
+	float rotateZ ;
+    // Start is called before the first frame update
+    void Start()
+    {
+		rotateZ = transform.rotation.z;
+
+	}
+
+
+	public void RotateClockWise()
+	{
+
+	}
+
+	public void RotateAntiClockWIse()
+	{
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		rotateZ = Mathf.Abs ( transform.rotation.z) + 10 * Time.deltaTime;
+		Debug.Log(rotateZ);
+		transform.Rotate(transform.rotation.x, transform.rotation.y, rotateZ);
+	}
+}
