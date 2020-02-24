5e13773d2f796ea5e7efc6cda2abf36fd5a84b55
Habibur Rahman <habibur.rahman@dreamonline.co.jp>
2020-02-24 16:03:48 +0600

    Enemy can rotate  towards player

---
+++ Assets/Script/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public GameObject playerObj;
+
+    void Start()
+    {
+        // transform.LookAt(playerObj.transform);
+        look();
+    }
+    private void Update()
+    {
+        //look();
+    }
+
+    void look()
+    {
+        Vector3 dir = playerObj.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+}
