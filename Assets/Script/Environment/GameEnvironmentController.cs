24a79d04311c4a73ad41173ab1bdd8cb29d74c91
oviebd <oviecse48@gmail.com>
2020-05-01 23:41:19 +0600

    Fixed Joystick bug

--- Assets/Script/Environment/GameEnvironmentController.cs
+++ Assets/Script/Environment/GameEnvironmentController.cs
@@ -56,6 +56,8 @@ public class GameEnvironmentController : MonoBehaviour
 		{
 			Destroy(_parentObjForInstantiatObjs.transform.GetChild(i).gameObject);
 		}
+		if (PlayerSpawnerController.instance.GetCurrentPlayerController() != null)
+			Destroy(PlayerSpawnerController.instance.GetCurrentPlayerController().gameObject);
 	}
 
 }
