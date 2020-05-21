3135cedc61fec5c16baaf264d902916200c6f5fe
oviebd <oviecse48@gmail.com>
2020-05-21 15:48:46 +0600

    Add enemies

--- Assets/Script/Enum/GameEnum.cs
+++ Assets/Script/Enum/GameEnum.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameEnum 
 {
-	public enum EnemyType { Type_1, Type_2,Type_3,Type_4,Type_5 }
+	public enum EnemyType { Type_1, Type_2,Type_3,Type_4,Type_5,Type_6,Type_7,Type_8,Type_9 }
 	public enum PlayerType { Type_1_Base, Type_2_Dozzer , Type_3_Healthy, Type_4_Shooter }
 	public enum CollectableType { Schild, Life,Update_Bullet_Frequency,Update_Bullet }
 	public enum GunType { GunType_1, GunType_2 }
